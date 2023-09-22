5f4c34217968285b57ea49ce40071681b39621e2
E-Knecht <142593414+E-Knecht@users.noreply.github.com>
2023-09-22 16:10:15 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+namespace Websrcaper
+{
+    class Programm
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                string StartURL = "https://www.daswetter.com/wetter_Biberstein-Europa-Schweiz-Aargau--1-188889.html";
+                var Httpclient = new HttpClient();
+                var html = Httpclient.GetStringAsync(StartURL).Result;
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(html);
+
+
+
+                var Temeraturelement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='dato-temperatura changeUnitT']");
+                var temperatur = Temeraturelement.InnerText.Trim();
+                Console.WriteLine(temperatur.Replace("&deg;", "°C"));
+
+                var wetterelement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='descripcion']");
+                var wetter = wetterelement.InnerText.Trim();
+                Console.WriteLine(wetter);
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
